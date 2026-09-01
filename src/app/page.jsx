29546e37680ca3ecff1e6b7cb0abe11d0e03796d
irfanxzone <irfanxzone121@gmail.com>
2026-09-02 00:14:56 +0500

    Refresh published dates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import { ALL_QUESTIONS } from "@/data/questions";
 import HomePageClient from "./HomePageClient";
 
 // Homepage metadata for SEO
-const HOME_SEO_TIMESTAMP = "2026-08-30T19:30:06+05:00";
+const HOME_SEO_TIMESTAMP = "2026-09-02T00:13:46+05:00";
 
 export const metadata = {
     title: "The Rice Purity Test",
